1162fa642dcf5b44cd4211394f6615a92747b2d0
renikQ <trog94@mail.ru>
2022-08-18 22:22:49 +0500

    completed homework second version

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,59 @@ public class Main {
     public static void main(String[] args) {
 
         // задача 1
-        int [] firstArray = new int [3];
+        int[] firstArray = new int[3];
         firstArray[0] = 1;
         firstArray[1] = 2;
         firstArray[2] = 3;
-        double [] secondArray = {1.57, 7.654, 9.986};
-        int [] thirdArray = new int[] {3, 2, 1};
+        double[] secondArray = {1.57, 7.654, 9.986};
+        int[] thirdArray = new int[]{3, 2, 1};
 
         // задача 2
-        System.out.println(firstArray[0] + ", " + firstArray[1] + ", " + firstArray[2]);
-        System.out.println(secondArray[0] + ", " + secondArray[1] + ", " + secondArray[2]);
-        System.out.println(thirdArray[0] + ", " + thirdArray[1] + ", " + thirdArray[2]);
+        for (int i = 0; i < firstArray.length; i++) {
+            if (i == firstArray.length - 1) {
+                System.out.println(firstArray[i]);
+                break;
+            }
+            System.out.print(firstArray[i] + ", ");
+        }
+        for (int i = 0; i < secondArray.length; i++) {
+            if (i == secondArray.length - 1) {
+                System.out.println(secondArray[i]);
+                break;
+            }
+            System.out.print(secondArray[i] + ", ");
+        }
+        for (int i = 0; i < thirdArray.length; i++) {
+            if (i == thirdArray.length - 1) {
+                System.out.println(thirdArray[i]);
+                break;
+            }
+            System.out.print(thirdArray[i] + ", ");
+        }
+
 
         // задача 3
-        System.out.println(firstArray[2] + ", " + firstArray[1] + ", " + firstArray[0]);
-        System.out.println(secondArray[2] + ", " + secondArray[1] + ", " + secondArray[0]);
-        System.out.println(thirdArray[2] + ", " + thirdArray[1] + ", " + thirdArray[0]);
+        for (int i = firstArray.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(firstArray[i]);
+                break;
+            }
+            System.out.print(firstArray[i] + ", ");
+        }
+        for (int i = secondArray.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(secondArray[i]);
+                break;
+            }
+            System.out.print(secondArray[i] + ", ");
+        }
+        for (int i = thirdArray.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(thirdArray[i]);
+                break;
+            }
+            System.out.print(thirdArray[i] + ", ");
+        }
 
         // задача 4
         for (int i = 0; i < firstArray.length; i++) {
@@ -25,6 +62,12 @@ public class Main {
                 firstArray[i]++;
             }
         }
-        System.out.println(firstArray[0] + ", " + firstArray[1] + ", " + firstArray[2]);
+        for (int i = 0; i < firstArray.length; i++) {
+            if (i == firstArray.length - 1) {
+                System.out.println(firstArray[i]);
+                break;
+            }
+            System.out.print(firstArray[i] + ", ");
+        }
     }
 }
